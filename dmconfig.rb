307250ff699c0befe97c0cb2559fabeb3ef28927
Robert Fletcher <cake@scribd.com>
2010-11-01 16:42:37 -0700

    removed unnecessary files

--- dmconfig.rb
+++
@@ -1,7 +0,0 @@
-require 'rubygems'
-require 'dm-core'
-require 'dm-migrations'
-require 'dm-validations'
-
-DataMapper::Logger.new($stdout, :debug)
-DataMapper.setup(:default, 'sqlite://' + Dir.pwd + '/rv.db')
